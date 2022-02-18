fd599f7147e9ebf5d90d4ff53568ca557faa9b27
Martin Endress <martin.endress@stud.uni-bamberg.de>
2022-02-18 15:50:41 +0100

    wip interaction simulation

--- manager/test_main.py
+++ manager/test_main.py
@@ -1,4 +1,29 @@
-print('test')
+import json
+import requests
 
+manager_url = "http://localhost:5000/"
 
-# journalctl -u privacyscanner_browser > privacyscanner_browser.txt
+print('enter URL')
+url = input()
+
+response = requests.post(manager_url + "start_instance", json={"url": url}).json()
+vnc_port = response['vnc_port']
+container_id = response['container_id']
+print('Container started. VNC PORT: %s' % vnc_port)
+
+print('Press any key to start scan.')
+input()
+
+response = requests.post(manager_url + "start_scan", json={"container_id": container_id}).json()
+print(json.dumps(response))
+
+while True:
+    print("Press any key to register interaction. 'Q' to quit.")
+    action = input()
+    if action == "Q":
+        break
+    response = requests.post(manager_url + "register_interaction", json={"container_id": container_id}).json()
+    print(json.dumps(response))
+
+response = requests.post(manager_url + "stop_scan", json={"container_id": container_id}).json()
+print(json.dumps(response))
