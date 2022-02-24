2378db2e941cc0970f4854206f58a4f4fb96b594
Martin Endress <martin.endress@stud.uni-bamberg.de>
2022-02-24 05:30:32 +0100

    update test script

--- manager/test_main.py
+++ manager/test_main.py
@@ -1,29 +1,39 @@
-import json
 import requests
 
-manager_url = "http://localhost:5000/"
 
-print('enter URL')
-url = input()
+def main():
+    manager_url = "http://localhost:5000/"
 
-response = requests.post(manager_url + "start_instance", json={"url": url}).json()
-vnc_port = response['vnc_port']
-container_id = response['container_id']
-print('Container started. VNC PORT: %s' % vnc_port)
+    print('Please enter URL to be scanned.')
+    url = input()
 
-print('Press any key to start scan.')
-input()
+    response = requests.post(manager_url + "start_instance", json={"url": url}).json()
+    vnc_port = response['vnc_port']
+    container_id = response['container_id']
+    print('Container started. VNC PORT: %s' % vnc_port)
 
-response = requests.post(manager_url + "start_scan", json={"container_id": container_id}).json()
-print(json.dumps(response))
+    print('Press any key to start scan.')
+    input()
 
-while True:
-    print("Press any key to register interaction. 'Q' to quit.")
-    action = input()
-    if action == "Q":
-        break
-    response = requests.post(manager_url + "register_interaction", json={"container_id": container_id}).json()
-    print(json.dumps(response))
+    response = requests.post(manager_url + "start_scan", json={"container_id": container_id})
+    if response.status_code != 200:
+        print(response)
 
-response = requests.post(manager_url + "stop_scan", json={"container_id": container_id}).json()
-print(json.dumps(response))
+    while True:
+        print("Press any key to register interaction. 'Q' to quit.")
+        action = input()
+        if action == "Q":
+            break
+        response = requests.post(manager_url + "register_interaction", json={"container_id": container_id})
+        if response.status_code != 200:
+            print(response)
+
+    response = requests.post(manager_url + "stop_scan", json={"container_id": container_id})
+    if response.status_code != 200:
+        print(response)
+
+    print('scan complete.')
+
+
+if __name__ == "__main__":
+    main()
