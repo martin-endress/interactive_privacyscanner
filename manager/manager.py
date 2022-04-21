d497f2ce4c3799ca034626aaae9c8167ae763931
Martin Endress <martin.endress@stud.uni-bamberg.de>
2022-04-21 17:52:31 +0200

    use proper server status responses

--- manager/manager.py
+++ manager/manager.py
@@ -3,7 +3,7 @@ import logging
 
 from flask import Flask, Response, request
 from podman.errors import PodmanError
-
+import time
 import logs
 from urllib.parse import urlparse
 from interactive_scanner import InteractiveScanner
@@ -25,12 +25,8 @@ app = Flask(__name__)
 def before_request():
     if not podman_available():
         logger.error('Podman not available. No action was performed.')
-        response_body = json.dumps(
-            {
-                "error": "Please start the podman service ('podman system service -t 0 &')" +
-                         " or enable systemd user unit podman.service"}
-        )
-        return Response(response_body, status=500, mimetype="application/json")
+        response_body = "Please start the podman service ('podman system service -t 0 &') or enable systemd user unit podman.service"
+        return Response(response_body, status=500, mimetype="text/plain")
     # continue with request
     return None
 
@@ -42,14 +38,14 @@ def start_instance():
     The subprocess is instructed through a message queue.
     """
     if request.json == None:
-        return Response(json.dumps({"error": 'Request must be a json containing the URL.'}), status=400)
+        return Response("Request must be a json containing the URL.", status=400)
     url = request.json['url']
     try:
         urlparse(url)
     except ValueError as e:
         msg = str(e)
         logger.error(msg)
-        return Response(json.dumps({"error": msg}), status=400)
+        return Response(msg, status=400)
 
     # Start container
     try:
@@ -57,15 +53,18 @@ def start_instance():
     except PodmanError as e:
         msg = str(e)
         logger.error(msg)
-        return Response(json.dumps({"error": msg}), status=503)
+        return Response(msg, status=503)
 
     # Start scanner thread
     scanner = InteractiveScanner(url, container.devtools_port, None)
     scanner.start()
     scanners[container.id] = scanner
 
+    time.sleep(2)
+
     # Respond
-    response_body = json.dumps({"vnc_port": container.vnc_port, "container_id": container.id})
+    response_body = json.dumps(
+        {"vnc_port": container.vnc_port, "container_id": container.id})
     return Response(response_body, status=200)
 
 
@@ -89,7 +88,8 @@ def register_interaction():
         scanner = scanners[container_id]
     except ValueError as e:
         return Response('Client Error: %s' % str(e), status=400)
-    scanner.put_msg(ScannerMessage(MessageType.RegisterInteraction, content=''))
+    scanner.put_msg(ScannerMessage(
+        MessageType.RegisterInteraction, content=''))
     return Response(status=200)
 
 
@@ -98,7 +98,8 @@ def stop_scan():
     try:
         container_id = get_container_id()
         scanner = scanners[container_id]
-        scanner.put_msg(ScannerMessage(MessageType.StopScan, content=container_id))
+        scanner.put_msg(ScannerMessage(
+            MessageType.StopScan, content=container_id))
         return Response('Scan completion initiated.', status=200)
     except PodmanError as e:
         return Response('Server Error: %s' % str(e), status=500)
@@ -108,7 +109,7 @@ def stop_scan():
 
 @app.route('/scan_status', methods=['GET'])
 def status():
-    return Response("", status=200)
+    return Response("server up", status=200)
 
 
 @app.route('/stop_all_scans', methods=['POST'])
