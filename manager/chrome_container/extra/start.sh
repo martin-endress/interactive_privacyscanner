30ab66395cfa2e20720ef45af213f773132d9070
Martin Endress <martin.endress@stud.uni-bamberg.de>
2022-04-23 16:27:15 +0200

    increase browser resolution

--- manager/chrome_container/extra/start.sh
+++ manager/chrome_container/extra/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Xvfb :1 -screen 0 1000x800x16 & x11vnc -rfbauth /home/chrome/.vnc/passwd -forever -shared -bg -display :1.0 & DISPLAY=:1.0
+Xvfb :1 -screen 0 1600x900x16 & x11vnc -rfbauth /home/chrome/.vnc/passwd -forever -shared -bg -display :1.0 & DISPLAY=:1.0
