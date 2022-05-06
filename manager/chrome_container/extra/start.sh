6b6e4905ee01f56660abc17d2f616373c3b9bd60
Martin Endress <martin.endress@stud.uni-bamberg.de>
2022-05-06 13:26:41 +0200

    optimize for 1080p displays

--- manager/chrome_container/extra/start.sh
+++ manager/chrome_container/extra/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Xvfb :1 -screen 0 1280x900x16 & x11vnc -rfbauth /home/chrome/.vnc/passwd -forever -shared -bg -display :1.0 & DISPLAY=:1.0
+Xvfb :1 -screen 0 1274x900x16 & x11vnc -rfbauth /home/chrome/.vnc/passwd -forever -shared -bg -display :1.0 & DISPLAY=:1.0
