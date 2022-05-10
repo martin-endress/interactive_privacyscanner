6ccfa0ffdbbe3de12bb24a0edc53964fa30fe94e
Martin Endress <martin.endress@stud.uni-bamberg.de>
2022-05-10 12:43:59 +0200

    requirements for api

---
+++ manager/browser2.py
@@ -0,0 +1,33 @@
+from chromedev import Browser
+
+class Browser:
+    pass
+
+class ChromeBrowser:
+    def __init__(self, debugging_port=9222):
+        self._debugging_port = debugging_port
+        self._browser_ip = "localhost"
+
+    async def __aenter__(self):
+        self.browser = Browser(
+            debugger_url="http://{}:{}".format(self._browser_ip, self._debugging_port),
+            debug=True,
+        )
+        await self.browser.start()
+        return self.browser
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.browser.close()
+
+
+
+"""
+requirements:
+
+setAutoAttach : Bool -> _
+newTab : Target
+ignoreInputs : Bool -> _
+navigate : URL -> _
+backgroundFetch : _
+
+"""
