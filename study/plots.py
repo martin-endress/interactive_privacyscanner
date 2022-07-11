a3036a4f3388670016133c68242d21979a5dbb41
Martin Endress <martin.endress@stud.uni-bamberg.de>
2022-07-11 18:06:21 +0200

    plots

---
+++ study/plots.py
@@ -0,0 +1,43 @@
+# libraries
+from pathlib import Path
+
+import matplotlib
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+out_path = Path("../../psi-exposee-master/degree-thesis/en/figures/study/")
+
+matplotlib.style.use('ggplot')
+
+# define colors
+ubblue = "#00457D"
+ubblue80 = "#336A97"
+ubblue60 = "#668FB1"
+ubblue40 = "#99B5CB"
+ubblue20 = "#CCDAE5"
+ubyellow = "#FFD300"
+ubyellow25 = "#FFF4BF"
+ubred = "#e6444F"
+ubgreen = "#97BF0D"
+color_pallete = [ubblue80, ubyellow, ubblue40, ubred, ubgreen, ubblue, ubblue60, ubblue20, ubyellow25]
+
+# read data from CSV
+df = pd.read_csv("results.csv", delimiter=',', na_values='-1')
+
+# transform data
+print(len(df))
+df['note'] = np.where(df['note'].isnull(), '', df.note)
+df.dropna(inplace=True)
+print(len(df))
+
+# create banner column
+conditions = [df['note'] == 'm', df['note'] == '', df['note'] == 'x']
+values = ['modal', 'banner', 'no banner']
+df['banner'] = np.select(conditions, values)
+
+# figure banner
+fig, ax = plt.subplots(figsize=(3, 2.5))
+df['banner'].value_counts().plot(ax=ax, kind='bar', color=color_pallete, rot=0, width=0.5)
+plt.savefig(out_path / 'banner_types.pdf')
+
